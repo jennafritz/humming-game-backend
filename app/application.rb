b0d2490d04f6973fdd13eef99e80f0a2495169a2
Katie Hyewon Choi <hyewonchoi31@gmail.com>
2021-06-02 10:41:28 -0400

    contue functionality

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,6 @@
-require 'pry'
+require "pry"
 
 class Application
-
   @@chosen_songs = []
 
   def call(env)
@@ -9,8 +8,8 @@ class Application
 
     if req.path.match(/hello/)
       send_hello
-    elsif req.path.match(/songs/) && req.get?
-     send_songs(req)
+    elsif req.path.match(/decades/) && req.get?
+      choose_decade(req)
     elsif req.path.match(/user_games/) && req.post?
       create_user_game(req)
     elsif req.path.match(/games/) && req.post?
@@ -22,13 +21,12 @@ class Application
         user_instance.username == username_search && user_instance.password == password_search
       end
       if searched_user
-        return [201, {"Content-Type" => "application/json"}, [searched_user.to_json]]
+        return [201, { "Content-Type" => "application/json" }, [searched_user.to_json]]
       else
-        return [201, {"Content-Type" => "application/json"}, [{ :message => "User does not exist" }.to_json]]
+        return [201, { "Content-Type" => "application/json" }, [{ :message => "User does not exist" }.to_json]]
       end
     elsif req.path.match(/users/) && req.post?
       create_user(req)
-
     else
       send_not_found
     end
@@ -59,29 +57,34 @@ class Application
     return [201, { "Content-Type" => "application/json" }, [new_user_instance.to_json]]
   end
 
-  def send_songs(req)
-  #   song_instance_array = Song.all
-  #   return [200, { "Content-Type" => "application/json" }, [song_instance_array.to_json]]
+  def choose_decade(req)
+    #   song_instance_array = Song.all
+    #   return [200, { "Content-Type" => "application/json" }, [song_instance_array.to_json]]
     decade_selected = req.params["year"].to_i
     decade_end = decade_selected + 9
-    songs_selected_by_decade = Song.where("year BETWEEN ? AND ?", decade_selected, decade_end )
+    songs_selected_by_decade = Song.where("year BETWEEN ? AND ?", decade_selected, decade_end)
     # songs_selected_by_decade = Song.all.select do |song_instance|
     #     song_instance.year >= decade_selected && song_instance.year <  decade_selected + 10
     # end
     songs_selected_by_decade.map do |song_instance|
-    @@chosen_songs << song_instance
+      @@chosen_songs << song_instance
     end
     # binding.pry
-    return [201, {"Content-Type" => "application/json"}, [{ :message => "#{decade_selected} added to songs" }.to_json]]    
+    return [201, { "Content-Type" => "application/json" }, [{ :message => "#{decade_selected} added to songs" }.to_json]]
+  end
+
+  def send_songs
+    four_random_songs = @@chosen_songs.sample(4)
+    return [201, { "Content-Type" => "application/json" }, [four_random_songs.to_json]]
   end
 
   # def self.chosen_songs=()
   #   @@chosen_songs
   # end
 
- def self.chosen_songs
- @@chosen_songs
- end
+  def self.chosen_songs
+    @@chosen_songs
+  end
 
   def send_not_found
     return [404, {}, ["Path not found!!!"]]
--- db/schema.rb
+++ db/schema.rb
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2021_05_31_190347) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password"
+    t.integer "point"
   end
 
 end
